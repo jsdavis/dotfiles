3895a31142733f8144bdcc5eff4a95a155bfede0
Jacob Davis <jacobd@yahoo-inc.com>
2016-06-23 11:55:58 -0700

    Missed setup in last commit

--- setup.sh
+++ setup.sh
@@ -30,6 +30,16 @@ fi
 ln -s $dir/.vimrc ~/ > /dev/null
 echo
 
+if hash tmux 2>/dev/null; then
+    echo ".tmux.conf..."
+    if [ -f ~/.tmux.conf ]; then
+        echo "File exists. Moving to .tmux.conf.old"
+        mv ~/.tmux.conf ~/.tmux.conf.old > /dev/null
+    fi
+    ln -s $dir/.tmux.conf ~/ > /dev/null
+    echo
+fi
+
 echo "Symlinks to the dotfiles have been created."
 read -p "Done. Press [Enter] to finish."
 

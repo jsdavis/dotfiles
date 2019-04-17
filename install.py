c00609efbfb5558c5a18a6ff162c1aab2b39f0b2
jsdavis <jsdavis1010@gmail.com>
2019-04-16 17:55:40 -0700

    Rewrote install script to do more things

--- install.py
+++ install.py
@@ -1,135 +1,259 @@
 #!/usr/bin/env python
 
-import __future__
 import os
 import glob
 import errno
 import shutil
 import filecmp
+import sys
+from functools import partial, wraps
 
 # Got this from StackOverflow and it works so don't touch it
 magic_powershell_command = 'powershell.exe -command "&{ start-process powershell -ArgumentList \'-executionpolicy bypass -command \"C:\\font\\Add-Font.ps1 C:\\font\\fonts\"\' -verb RunAs}"'
 
-def check_input(raw):
-    ans = raw_input(raw)
-    ans = ans.lower()
-    return not (ans == 'n' or ans == 'no')
 
-def system_type():
-    if os.path.isdir('/mnt/c/Windows'):
-        return 'WSL'
+class NotSupportedError(Exception):
+    pass
+
+
+def platform():
+    if sys.platform == 'darwin':
+        return 'mac'
+    elif os.path.isdir('/mnt/c/Windows'):
+        return 'wsl'
     else:
-        return 'Other'
+        return 'linux'
 
-def fonts_installed(font_dir):
-    installed_fonts = [os.path.basename(font) for font in glob.glob('/mnt/c/Windows/Fonts/*.ttf')]
-    attempted_fonts = [os.path.basename(font) for font in glob.glob(os.path.join(font_dir, '*.ttf'))]
-    for font in attempted_fonts:
-        if font not in installed_fonts:
-            return False
-    return True
 
-def manual_font_installation():
-    print("\tAn error occurred installing fonts. Opening folder for manual installation")
-    os.system('explorer.exe fonts')
+def ask_user(question):
+    ans = 'ans'
+    while ans and not (ans.startswith('n') or ans.startswith('y')):
+        ans = raw_input(question)
+        ans = ans.strip().lower()
+    return not (ans.startswith('n'))
 
-def dotfiles():
-    print("\nSetting up dotfiles...\n")
 
-    # Get absolute path of current directory
-    curr_dir = os.path.dirname(os.path.abspath(__file__))
+def install(func=None, platforms=None, ask=None, name=None, order=10):
+    if func is None:
+        return partial(install, platforms=platforms, ask=ask, name=None, order=order)
 
-    # For Windows filesystem compatability
-    home = os.path.expanduser("~")
+    if platforms is None:
+        platforms = ('wsl', 'mac', 'linux')
 
-    # Get list of dotfiles in rc directory
-    files = glob.glob(os.path.join(curr_dir,"rc",".*"))
+    func.is_install_step = True
+    func.supported = platform() in platforms
+    func.ask = '\n{} (y)/n '.format(ask) if ask is not None else ask
+    func.order = order
+    func.pretty_name = name if name is not None else func.__name__.replace('_', ' ').title()
 
-    # Create symlinks to found dotfiles as necessary
-    for dotfile in files:
-        filename = os.path.basename(dotfile)
-        homeLink = os.path.join(home,filename)
+    @wraps(func)
+    def decorator(*args, **kwargs):
+        if not func.supported:
+            raise NotSupportedError('This operation is not supported on the {} platform'.format(platform()))
 
-        # Don't clobber dotfiles that exist already
+        func.run = ask_user(func.ask) if func.ask is not None else True
+        if func.run:
+            return func(*args, **kwargs)
+        else:
+            print('{} will not be installed.'.format(func.pretty_name))
+    return decorator
+
+
+class Installer(object):
+    def __init__(self):
+        self.platform = platform()
+        self.dir = os.path.dirname(os.path.abspath(__file__))
+
+        self.rc_dir = os.path.join(self.dir, 'rc')
+        self.font_dir = os.path.join(self.dir, 'bin', 'font')
+
+    def run(self):
+        for install in self.get_installs():
+            install(self)
+
+        print("\nInstallation complete!")
+
+    def run_cmd(self, cmd):
+        print('-' * 50)
+        print(cmd)
+        os.system(cmd)
+        print('-' * 50)
+
+    @classmethod
+    def get_installs(cls, supported_only=True):
+        installs = (getattr(cls, func) for func in dir(cls) if cls._is_install(func))
+        if supported_only:
+            installs = (install for install in installs if install.supported)
+        return sorted(installs, key=lambda x: x.order)
+
+    @classmethod
+    def _is_install(cls, method):
         try:
-            # Don't backup dotfiles that are the same
-            if not filecmp.cmp(homeLink, dotfile, False):
-                os.rename(homeLink, homeLink + ".bak")
-                print("\t{0} exists. Moved to {0}.bak".format(filename))
+            if not callable(method):
+                method = getattr(cls, method)
+            return method.is_install_step
+        except AttributeError:
+            return False
 
-            # Delete original if same. Not efficient, but cleaner logic
-            else:
-                os.remove(homeLink)
+    @install(ask='Do you want to install dotfiles?', order=0)
+    def dotfiles(self):
+        print("\nSetting up dotfiles...\n")
 
-        except OSError as err:
-            if errno.errorcode[err.errno] == 'EISDIR':
-                if os.path.islink(homeLink):
-                    os.unlink(homeLink)
+        # For Windows filesystem compatability
+        home = os.path.expanduser("~")
+
+        # Get list of dotfiles in rc directory
+        files = glob.glob(os.path.join(self.rc_dir, ".*"))
+
+        # Create symlinks to found dotfiles as necessary
+        for dotfile in files:
+            filename = os.path.basename(dotfile)
+            homeLink = os.path.join(home, filename)
+
+            # If symlink already exists, first remove it
+            if os.path.islink(homeLink):
+                os.unlink(homeLink)
+
+            # Don't clobber dotfiles that exist already
+            try:
+                # Don't backup dotfiles that are the same
+                if not filecmp.cmp(homeLink, dotfile, False):
+                    os.rename(homeLink, homeLink + ".bak")
+                    print("\t{0} exists. Moved to {0}.bak".format(filename))
+
+                # Delete original if same. Not efficient, but cleaner logic
                 else:
-                    shutil.rmtree(homeLink)
+                    os.remove(homeLink)
 
-        if os.path.islink(homeLink):
-            os.unlink(homeLink)
-        os.symlink(dotfile, homeLink)
+            except OSError as err:
+                if errno.errorcode[err.errno] == 'EISDIR':
+                    if os.path.islink(homeLink):
+                        os.unlink(homeLink)
+                    else:
+                        shutil.rmtree(homeLink)
 
+            os.symlink(dotfile, homeLink)
 
-    # We're done!
-    print("Dotfile setup complete.")
+        # We're done!
+        print("Dotfile setup complete.")
 
-def install_fonts():
-    # Get absolute path of current directory
-    curr_dir = os.path.dirname(os.path.abspath(__file__))
+    @install(platforms=['wsl', 'mac'], ask='Do you want to install fonts?')
+    def fonts(self):
+        if self.platform == 'wsl':
+            return self._wsl_fonts()
+        elif self.platform == 'mac':
+            return self._mac_fonts()
 
-    # This only works on WSL for now
-    if system_type() == 'WSL':
+    def _mac_fonts(self):
+        font_install_dir = '/Library/Fonts'
+
+        for font in glob.glob(os.path.join(self.font_dir, 'fonts/*.ttf')):
+            font_file = os.path.basename(font)
+            try:
+                shutil.copyfile(font, os.path.join(font_install_dir, font_file))
+            except OSError as err:
+                if errno.errorcode[err.errno] == 'EEXIST':
+                    print('{} is already installed'.format(font_file))
+                else:
+                    raise
+            else:
+                print('Installed font {}'.format(font_file))
+
+    def _wsl_fonts(self):
         c_font = '/mnt/c/font'
 
         # First check if fonts are already installed
-        if fonts_installed(c_font):
+        if self._fonts_installed_wsl(c_font):
             print("\tFonts are already installed.")
             return
 
         try:
             # Copy fonts to a folder on the Windows filesystem
-            shutil.copytree(os.path.join(curr_dir, 'scripts/font'), c_font)
+            shutil.copytree(os.path.join(self.font_dir), c_font)
             print("\tFonts copied to {0} directory".format(c_font))
 
             os.chdir(c_font)
-            if os.system(magic_powershell_command) == 0:
-                if fonts_installed(c_font):
+            if self.run_cmd(magic_powershell_command) == 0:
+                if self._fonts_installed_wsl(c_font):
                     print("\tFonts successfully installed\n\tCleaning up...")
-                    os.chdir(curr_dir)
+                    os.chdir(self.dir)
                     shutil.rmtree(c_font)
                 else:
-                    manual_font_installation()
-                    os.chdir(curr_dir)
+                    self._manual_font_installation_wsl()
+                    os.chdir(self.dir)
             else:
-                manual_font_installation()
-                os.chdir(curr_dir)
+                self._manual_font_installation_wsl()
+                os.chdir(self.dir)
 
         except OSError as err:
             if errno.errorcode[err.errno] == 'EEXIST':
-                print("{0} directory already exists. Nothing was copied".format(c_font))
+                print("{} directory already exists. Nothing was copied".format(c_font))
             else:
                 raise
-    else:
-        print("\nThere is no support for non-WSL systems at this time.")
+
+    def _fonts_installed_wsl(self, font_dir):
+        installed_fonts = [os.path.basename(font) for font in glob.glob('/mnt/c/Windows/Fonts/*.ttf')]
+        attempted_fonts = [os.path.basename(font) for font in glob.glob(os.path.join(font_dir, '*.ttf'))]
+        for font in attempted_fonts:
+            if font not in installed_fonts:
+                return False
+        return True
+
+    def _manual_font_installation_wsl(self):
+        print("\tAn error occurred installing fonts. Opening folder for manual installation")
+        self.run_cmd('explorer.exe fonts')
+
+    @install(platforms=['mac'], ask='Configure MacOS settings?')
+    def mac_settings(self):
+        commands = [
+            'defaults write com.apple.finder AppleShowAllFiles YES',  # Show hidden files
+            'defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false',  # No accents on holding keys
+            'defaults write .GlobalPreferences com.apple.mouse.scaling -1',  # Turn off mouse acceleration
+            'defaults write .GlobalPreferences com.apple.scrollwheel.scaling -1'  # Turn off scroll acceleration
+        ]
+        for cmd in commands:
+            self.run_cmd(cmd)
+
+    @install(platforms=['mac'], ask='Install MacOS packages?', order=1)
+    def mac_apps(self):
+        commands = [
+            # XCode dev tools, includes git
+            'xcode-select --install',
+
+            # Homebrew and Homebrew cask.
+            '/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"',
+            'brew update',
+            'brew analytics off',
+            'brew tap caskroom/cask'
+        ]
+        for cmd in commands:
+            self.run_cmd(cmd)
+
+    @install(platforms=['mac'], ask='Install Homebrew packages?')
+    def brew_apps(self):
+        packages = ['pyenv', 'wget', 'rename', 'fasd']
+        cmd = 'brew install {}'.format(' '.join(p for p in packages))
+        self.run_cmd(cmd)
+
+    @install(platforms=['mac'], ask='Install Homebrew casks?', order=100)
+    def brew_casks(self):
+        casks = ['iterm2', 'sublime-text', 'google-chrome', 'slack', 'postman', 'docker', 'spotify']
+        cmd = 'brew cask install {}'.format(' '.join(c for c in casks))
+        self.run_cmd(cmd)
+
+    @install(ask='Install NVM?')
+    def nvm(self):
+        cmd = \
+            'git clone https://github.com/creationix/nvm.git {0} && ' \
+            'cd {0} && ' \
+            'git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`; ' \
+            'cd {1}'.format(os.path.join(os.path.expanduser('~'), '.nvm'), self.dir)
+        self.run_cmd(cmd)
 
 
 def main():
-    # Do we want to install dotfiles?
-    if check_input('\nInstall dotfiles? (y) '):
-        dotfiles()
-    else:
-        print("Dotfiles will not be installed.")
+    return Installer().run()
 
-    # Do we want to install fonts?
-    if check_input('\nInstall powerline fonts? (y) '):
-        install_fonts()
-    else:
-        print("No fonts will be installed")
-
-    print("\nInstallation complete!")
 
 if __name__ == '__main__':
     main()

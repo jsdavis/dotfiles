47c3f50a58d141a28249d9134f6d49a3da86c0af
jsdavis <jsdavis1010@gmail.com>
2020-07-09 16:48:34 -0700

    Remove unused oh-my-zsh functions

--- lib/zsh/lib/functions.zsh
+++ lib/zsh/lib/functions.zsh
@@ -2,14 +2,6 @@ function zsh_stats() {
   fc -l 1 | awk '{CMD[$2]++;count++;}END { for (a in CMD)print CMD[a] " " CMD[a]/count*100 "% " a;}' | grep -v "./" | column -c3 -s " " -t | sort -nr | nl |  head -n20
 }
 
-function uninstall_oh_my_zsh() {
-  env ZSH=$ZSH sh $ZSH/tools/uninstall.sh
-}
-
-function upgrade_oh_my_zsh() {
-  env ZSH=$ZSH sh $ZSH/tools/upgrade.sh
-}
-
 function take() {
   mkdir -p $1
   cd $1
